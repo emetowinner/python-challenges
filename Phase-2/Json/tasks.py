a0075b8f0d56bda160856c4f55f4d9ac09dff1c8
emetowinner <emetowinner@gmail.com>
2020-07-29 11:44:52 +0100

    Updated tasks

--- Phase-2/Json/tasks.py
+++ Phase-2/Json/tasks.py
@@ -1,5 +1,3 @@
-"""
-
 1. Write a Python program to convert JSON data to Python object.
 
 
@@ -25,7 +23,3 @@
 
 
 9. Write a Python program to access only unique key value of a Python object.
-
-
-
-"""
